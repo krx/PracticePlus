5a31b3033ce4a7f9aecc83faa1a9819d4019b2c0
LDeathh <42755317+LDeathh@users.noreply.github.com>
2020-05-08 20:37:26 +0100

    Update buildBMBF.ps1

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -7,4 +7,4 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libnjschanger.so","./bmbfmod.json" -DestinationPath "./njschanger_v0.1.0.zip"
+Compress-Archive -Path "./libs/arm64-v8a/libpracticeplus.so","./bmbfmod.json" -DestinationPath "./practiceplus_v0.1.0.zip"
